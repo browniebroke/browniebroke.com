3f6bdc5a09924e1272f85c2819eecde1d77abd65
Bruno Alla <alla.brunoo@gmail.com>
2019-07-07 12:49:12 +0100

    Refactor gatsby-node to use async/await & clean things up

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,76 +1,67 @@
-const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 const slugify = require('slugify')
 
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
-
-  // Create pages for each post
-  const blogPost = path.resolve(`./src/templates/post.js`)
-  return graphql(
-    `
-      {
-        posts: allMarkdownRemark(
-          sort: { fields: [frontmatter___date], order: DESC }
-          limit: 1000
-        ) {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-                description
-              }
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  // Run query to get data
+  const result = await graphql(`
+    {
+      posts: allMarkdownRemark(
+        sort: { fields: [frontmatter___date], order: DESC }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+              description
             }
           }
         }
-        tags: allMarkdownRemark {
-          group(field: frontmatter___tags) {
-            tag: fieldValue
-          }
+      }
+      tags: allMarkdownRemark {
+        group(field: frontmatter___tags) {
+          tag: fieldValue
         }
       }
-    `
-  ).then(result => {
-    if (result.errors) {
-      throw result.errors
     }
+  `)
 
-    // Create blog posts pages.
-    const posts = result.data.posts.edges
+  if (result.errors) {
+    reporter.panic(`Error creating posts`, reporter.errors)
+  }
 
-    posts.forEach((post, index) => {
-      const previous = index === posts.length - 1 ? null : posts[index + 1].node
-      const next = index === 0 ? null : posts[index - 1].node
+  // Create blog posts pages.
+  const posts = result.data.posts.edges
 
-      createPage({
-        path: post.node.fields.slug,
-        component: blogPost,
-        context: {
-          slug: post.node.fields.slug,
-          previous,
-          next,
-        },
-      })
+  posts.forEach((post, index) => {
+    const previous = index === posts.length - 1 ? null : posts[index + 1].node
+    const next = index === 0 ? null : posts[index - 1].node
+
+    actions.createPage({
+      path: post.node.fields.slug,
+      component: require.resolve(`./src/templates/post.js`),
+      context: {
+        slug: post.node.fields.slug,
+        previous,
+        next,
+      },
     })
+  })
 
-    // Create tag pages
-    const tagPage = path.resolve(`./src/templates/tag_page.js`)
-    const tagsArray = result.data.tags.group
+  // Create tag pages
+  const tagsArray = result.data.tags.group
 
-    tagsArray.forEach((tagObj, index) => {
-      createPage({
-        path: `tags/${slugify(tagObj.tag)}`,
-        component: tagPage,
-        context: {
-          tag: tagObj.tag,
-        },
-      })
+  tagsArray.forEach(tagObj => {
+    actions.createPage({
+      path: `tags/${slugify(tagObj.tag)}`,
+      component: require.resolve(`./src/templates/tag_page.js`),
+      context: {
+        tag: tagObj.tag,
+      },
     })
-
-    return null
   })
 }
 
