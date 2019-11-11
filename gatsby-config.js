c7181f39f2dacd225ac6e6b5cca7643fc05b4e98
Bruno Alla <alla.brunoo@gmail.com>
2019-11-11 21:34:19 +0000

    More CSP tweaks

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,11 +74,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-csp`,
       options: {
+        mergeScriptHashes: false,
+        mergeStyleHashes: false,
         directives: {
           'script-src': "'self' 'unsafe-inline' data: www.google-analytics.com",
-          'style-src':
-            "'self' 'unsafe-inline' fonts.googleapis.com fonts.gstatic.com",
-          'img-src': "'self' data: www.google-analytics.com",
+          'style-src': `'self' 'unsafe-inline' fonts.googleapis.com fonts.gstatic.com`,
+          'img-src': `'self' data: www.google-analytics.com`,
+          'font-src': `'self' data: fonts.gstatic.com`,
         },
       },
     },
