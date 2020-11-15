c91dff0b5185e1d853ec99df012e41e78393d8f6
Bruno Alla <browniebroke@users.noreply.github.com>
2020-11-15 17:25:50 +0000

    Fix build

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,7 +63,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GA_TRACKING_ID || '',
+        trackingId: process.env.GA_TRACKING_ID,
       },
     },
     {
