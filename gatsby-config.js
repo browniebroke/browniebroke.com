1deff7c50696cdeda885621e4315589f1ea0fc97
Bruno Alla <alla.brunoo@gmail.com>
2022-04-05 15:18:45 +0000

    fix: set ga tracking ID for prod deploys

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,13 @@ const baseUrl =
     ? `https://deploy-preview-${process.env.REVIEW_ID}--browniebroke.netlify.app`
     : `https://browniebroke.com`
 
+const gaTrackingId =
+  process.env.NODE_ENV === 'development' || process.env.REVIEW_ID
+    ? 'G-xxx'
+    : 'G-DLGHEH0LX2'
+
+console.log('GA Tracking ID %s', gaTrackingId)
+
 module.exports = {
   siteMetadata: {
     title: `browniebroke.com`,
@@ -66,7 +73,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          process.env.GA_TRACKING_ID || 'G-xxx', // Google Analytics / GA
+          gaTrackingId, // Google Analytics / GA
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
