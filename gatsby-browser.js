2ab2b0dddcad990cd465ca04d8488ecd7aec5817
Bruno Alla <alla.brunoo@gmail.com>
2025-01-16 19:37:01 +0000

    Remove everything

--- gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-exports.onInitialClientRender = () => {
-  // Fonts
-  require('@fontsource/patua-one')
-  require('@fontsource/cabin-condensed')
-  require('@fontsource/cabin-condensed/700.css')
-  // PrismJS theme for code samples
-  require('prismjs/themes/prism.css')
-}
--- src/components/see-more.tsx
+++
@@ -1,9 +0,0 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
-import { AcceptsChildren } from './types'
-
-export const SeeMoreStyles: React.FC<AcceptsChildren> = ({ children }) => (
-  <Box as="p" m={12} textAlign={['center']}>
-    {children}
-  </Box>
-)
