39778ad826047ebf66675a289ccee42979a00929
Bruno Alla <alla.brunoo@gmail.com>
2025-01-18 13:45:48 +0000

    Put the avatar component on the welcome Astro page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx(), react()]
 });
