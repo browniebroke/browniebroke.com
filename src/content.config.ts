7d068f2dddca66ccc023613a3b26180ababdd5b5
Bruno Alla <alla.brunoo@gmail.com>
2025-01-28 20:23:31 +0000

    Start setting up TILs index page

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,13 +2,25 @@ import { z, defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
-  loader: glob({ pattern: '**/*.md', base: './src/pages/blog' }),
+  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/pages/blog' }),
   schema: z.object({
+    date: z.coerce.date(),
+    author: z.string(),
     title: z.string(),
-    permalink: z.string().optional(),
+    description: z.string(),
+    header_image: z.string().optional(),
+    tags: z.array(z.string())
+  }),
+});
+
+const tils = defineCollection({
+  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/pages/tils' }),
+  schema: z.object({
+    date: z.coerce.date(),
+    title: z.string(),
   }),
 });
 
 // Expose your defined collection to Astro
 // with the `collections` export
-export const collections = { blog };
+export const collections = { blog, tils };
