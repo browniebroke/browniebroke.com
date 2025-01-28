462b75717cef62f1981607362e4859223827a1c4
Bruno Alla <alla.brunoo@gmail.com>
2025-01-28 22:49:18 +0000

    Type fixes

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,12 +1,15 @@
 import slugify from 'slugify';
 
-function makeUrlFrom(prefix, slug) {
+function makeUrlFrom(prefix: string, slug: string): string {
   // Remove the date prefix from the slug
   const matches = slug.match(/\/\d{4}-\d{2}-\d{2}-(.*)\//)
+  if (matches === null) {
+    throw new Error(`Invalid slug: ${slug}`)
+  }
   const dateLessSlug = matches[1]
   return `/${prefix}/${dateLessSlug}/`
 }
 
-export const makePostUrl = (slug) => makeUrlFrom('blog', slug)
-export const makeTagUrl = (tagName) => `/blog/tags/${slugify(tagName)}`
-export const makeTILUrl = (slug) => makeUrlFrom('tils', slug)
+export const makePostUrl = (slug: string) => makeUrlFrom('blog', slug)
+export const makeTagUrl = (tagName: string) => `/blog/tags/${slugify(tagName)}`
+export const makeTILUrl = (slug: string) => makeUrlFrom('tils', slug)
