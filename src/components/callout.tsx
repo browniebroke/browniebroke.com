d59b982b341ff5989868d640eccf16afc75aaa86
Bruno Alla <alla.brunoo@gmail.com>
2025-01-31 13:11:14 +0000

    Make icon dynamic in callout component

--- src/components/callout.tsx
+++ src/components/callout.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import { FaInfoCircle } from 'react-icons/fa'
+import {
+  FaInfoCircle,
+  FaExclamationTriangle,
+  FaTimesCircle,
+} from 'react-icons/fa'
 import { type AcceptsChildren } from './types'
 
 interface CalloutProps extends AcceptsChildren {
@@ -14,10 +18,28 @@ export const Callout = ({ level, title, children }: CalloutProps) => {
   if (title === undefined) {
     title = level
   }
+
+  let Icon = FaInfoCircle
+
+  switch (level) {
+    case 'note':
+      Icon = FaInfoCircle
+      break
+    case 'info':
+      Icon = FaInfoCircle
+      break
+    case 'warning':
+      Icon = FaExclamationTriangle
+      break
+    case 'error':
+      Icon = FaTimesCircle
+      break
+  }
+
   return (
     <div className="flex items-start p-4 rounded-md bg-blue-100 border-l-4 border-blue-500">
       <div className="flex-shrink-0 mt-0.5 text-blue-500">
-        <FaInfoCircle className="h-5 w-5"></FaInfoCircle>
+        <Icon className="h-5 w-5"></Icon>
       </div>
       <div className="ml-3">
         <h3 className="mt-0.5 capitalize">{title}</h3>
