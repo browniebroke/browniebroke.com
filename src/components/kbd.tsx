99531a6621aa541bd9aa142268febb08eaa4371e
Bruno Alla <browniebroke@users.noreply.github.com>
2025-01-31 11:32:02 +0000

    Replace Kbd component by Tailwind styling of the HTML tag (#1829)

--- src/components/kbd.tsx
+++
@@ -1,8 +0,0 @@
-import { AcceptsChildren } from './types'
-import React from 'react'
-
-export const Kbd = ({ children }: AcceptsChildren) => (
-  <kbd className="px-2 py-1 text-sm font-mono bg-gray-100 border border-gray-300 rounded">
-    {children}
-  </kbd>
-)
