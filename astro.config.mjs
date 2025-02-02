227888ad37a6209d27f5aebdccbd944da68fd4f6
Bruno Alla <alla.brunoo@gmail.com>
2025-02-02 15:19:24 +0000

    Add time to read to posts and improve date formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,25 +5,12 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-
-const tilDefaultLayout = () => {
-  // @ts-ignore
-  return function (_, file) {
-    // get keys of the file.data.astro.frontmatter
-    const frontMatterKeys = Object.keys(file.data.astro.frontmatter);
-    // If 'author' is present in frontmatter, use blog post layout
-    // Otherwise, use TIL Layout
-    const defaultLayout = frontMatterKeys.includes("author") ? "../../../layouts/BlogPostLayout.astro" : "../../layouts/TilsLayout.astro";
-    console.log(`Using default layout: '${defaultLayout}'`);
-    file.data.astro.frontmatter.layout =
-      file.data.astro.frontmatter.layout || defaultLayout;
-  };
-};
+import { getDefaultLayout, remarkReadingTime } from './src/utils/remarkPlugins.mjs'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), react(), tailwind()],
   markdown: {
-    remarkPlugins: [tilDefaultLayout],
+    remarkPlugins: [getDefaultLayout, remarkReadingTime],
   }
 });
---
+++ src/utils/remarkPlugins.mjs
@@ -0,0 +1,27 @@
+import getReadingTime from 'reading-time';
+import { toString } from 'mdast-util-to-string';
+
+
+export const getDefaultLayout = () => {
+  // @ts-ignore
+  return function (_, file) {
+    // get keys of the file.data.astro.frontmatter
+    const frontMatterKeys = Object.keys(file.data.astro.frontmatter);
+    // If 'author' is present in frontmatter, use blog post layout
+    // Otherwise, use TIL Layout
+    const defaultLayout = frontMatterKeys.includes("author") ? "../../../layouts/BlogPostLayout.astro" : "../../layouts/TilsLayout.astro";
+    console.log(`Using default layout: '${defaultLayout}'`);
+    file.data.astro.frontmatter.layout =
+      file.data.astro.frontmatter.layout || defaultLayout;
+  };
+};
+
+export const remarkReadingTime = () => {
+  return function (tree, file) {
+    const textOnPage = toString(tree);
+    const readingTime = getReadingTime(textOnPage);
+    // readingTime.text will give us minutes read as a friendly string,
+    // i.e. "3 min read"
+    file.data.astro.frontmatter.minutesRead = readingTime.text;
+  };
+}
