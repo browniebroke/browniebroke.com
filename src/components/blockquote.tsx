4e6956cc78bdf928e1a9b9c2ab4ba83fc5de6cd8
Bruno Alla <alla.brunoo@gmail.com>
2025-02-06 20:14:09 +0000

    Remove unused component

--- src/components/blockquote.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { AcceptsChildren } from "./types";
-
-export const BlockQuote = ({ children }: AcceptsChildren) => (
-  <blockquote className="pl-4 mb-4 border-l-[5px] border-l-gray-200 italic">
-    {children}
-  </blockquote>
-);
