70016aa42de67deec0390e412394b5de7f9ab8da
Bruno Alla <alla.brunoo@gmail.com>
2025-02-06 20:32:46 +0000

    Fix tag component

--- src/components/tag.tsx
+++ src/components/tag.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "gatsby";
 import type { AcceptsChildren } from "./types.ts";
 
 interface TagProps extends AcceptsChildren {
@@ -8,10 +7,10 @@ interface TagProps extends AcceptsChildren {
 
 export const Tag = ({ to, children }: TagProps) => {
   return (
-    <Link to={to}>
+    <a href={to}>
       <span className="inline-block px-2 py-1 text-sm text-white font-medium bg-gray-500 rounded hover:bg-gray-600">
         {children}
       </span>
-    </Link>
+    </a>
   );
 };
