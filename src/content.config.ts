486a4d4d67219130c464d9dad2d5b107cd7a271c
Bruno Alla <alla.brunoo@gmail.com>
2025-02-08 11:40:09 +0000

    Setup cover image

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,14 +3,15 @@ import { glob } from "astro/loaders";
 
 const blog = defineCollection({
   loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/pages/blog" }),
-  schema: z.object({
-    date: z.coerce.date(),
-    author: z.string(),
-    title: z.string(),
-    description: z.string(),
-    header_image: z.string().optional(),
-    tags: z.array(z.string()),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      date: z.coerce.date(),
+      author: z.string(),
+      title: z.string(),
+      description: z.string(),
+      header_image: image().optional(),
+      tags: z.array(z.string()),
+    }),
 });
 
 const tils = defineCollection({
