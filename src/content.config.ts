24a02c0f76ffb0cccbf42c1172d28ee65ced4987
Bruno Alla <alla.brunoo@gmail.com>
2025-02-08 11:04:11 +0000

    Unused import

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,4 @@
-import { z, defineCollection, reference } from "astro:content";
+import { z, defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
 
 const blog = defineCollection({
