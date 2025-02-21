955f76afefe23da9304004910a4d24caf643e898
Bruno Alla <alla.brunoo@gmail.com>
2025-02-21 19:25:23 +0000

    Fix post title component

--- src/components/post-title.tsx
+++ src/components/post-title.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import type { AcceptsChildren } from "./types";
 
-export const PostTitle = ({ children }: AcceptsChildren) => {
-  <h1 className="text-3xl font-bold mt-8 mb-4">{children}</h1>;
-};
+export const PostTitle = ({ children }: AcceptsChildren) => (
+  <h1 className="text-3xl font-bold mt-8 mb-4">{children}</h1>
+);
